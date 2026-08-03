ef4827bf3d072578cf2a2400af655051603e087f
darkirby <darkirby@tuta.com>
2026-08-03 15:23:29 +0200

    Improve core sshd config to customize root UI addr

--- core/config/entrypoint.sh
+++ core/config/entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+chown root:root /etc/ssh/ssh_host_ed25519_key /etc/ssh/ssh_host_ed25519_key.pub /root/.ssh/authorized_keys
+chown -R git:git /home/git/git-shell-commands
+
+echo "AllowUsers root@$UI_ADDR" > /etc/ssh/sshd_config.d/90_allow_root.conf 
+
 exec /usr/sbin/sshd -D -e "$@"
