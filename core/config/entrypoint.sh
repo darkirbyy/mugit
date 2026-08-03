814cabe8e26b071d97e283a826dd0a1521bb9d24
darkirby <darkirby@tuta.com>
2026-08-03 20:27:20 +0200

    Add update script for the core

--- core/config/entrypoint.sh
+++ core/config/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 chown root:root /etc/ssh/ssh_host_ed25519_key /etc/ssh/ssh_host_ed25519_key.pub /root/.ssh/authorized_keys
+chmod 600 /etc/ssh/ssh_host_ed25519_key
 chown -R git:git /home/git/git-shell-commands
 
 echo "AllowUsers root@$UI_ADDR" > /etc/ssh/sshd_config.d/90_allow_root.conf 
---
+++ core/update.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Checking if first argument is present
+if [ -z "$1" ]; then
+    echo "No version provided, aborting."
+    exit 1
+fi
+
+# Checking if first argument is a version (semver format)
+if  ! echo "$1" | grep -Eq "^[0-9]+\.[0-9]+\.[0-9]+$"; then
+    echo "Version not im semver format (X.X.X), aborting."
+    exit 1
+fi
+
+echo "Updating the core version..."
+
+# Stop the current mugit core container
+docker compose down
+
+# Checkout the version given in argument
+git checkout "v$1"
+
+# Reuild and restart the container
+docker compose up --build -d
+
+echo "Done."
+exit 0
