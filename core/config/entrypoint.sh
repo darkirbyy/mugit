ff2e5e67ea8d8455f44932a48e1a28cb6f847a6d
darkirby <darkirby@tuta.com>
2026-08-06 18:25:13 +0200

    Improve core sshd and gitconfig

--- core/config/entrypoint.sh
+++ core/config/entrypoint.sh
@@ -5,5 +5,6 @@ chmod 600 /etc/ssh/ssh_host_ed25519_key
 chown -R git:git /home/git/git-shell-commands
 
 echo "AllowUsers root@$UI_ADDR" > /etc/ssh/sshd_config.d/90_allow_root.conf 
+echo "REPO_MAX_SIZE_MO=$REPO_MAX_SIZE_MO" > /etc/git/hooks.conf
 
 exec /usr/sbin/sshd -D -e "$@"
