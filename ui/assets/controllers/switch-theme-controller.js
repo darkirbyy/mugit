683fb68d17245c7e37c672176b28f18839d3972e
darkirby <darkirby@tuta.com>
2026-08-08 22:44:23 +0200

    Improve switch theme toggle buttons

--- ui/assets/controllers/switch-theme-controller.js
+++ ui/assets/controllers/switch-theme-controller.js
@@ -4,10 +4,16 @@ import { Controller } from '@hotwired/stimulus';
  * Stimulus controller that MUST be placed on the switch theme modal
  */
 export default class extends Controller {
-  static targets = ['light', 'dark', 'auto'];
+  static targets = ['button', 'light', 'dark', 'auto'];
+  static values = {
+    classActive: String,
+    classInactive: String,
+  };
 
   initialize() {
     this.storagePath = 'mugit/theme';
+    this.classActive = this.classActiveValue.split(' ');
+    this.classInactive = this.classInactiveValue.split(' ');
   }
 
   connect() {
@@ -15,26 +21,16 @@ export default class extends Controller {
 
     this.lightTarget.addEventListener('click', () => {
       this.setLocalTheme('light');
+      this.applyTheme();
     });
     this.darkTarget.addEventListener('click', () => {
       this.setLocalTheme('dark');
+      this.applyTheme();
     });
     this.autoTarget.addEventListener('click', () => {
       this.setLocalTheme('auto');
+      this.applyTheme();
     });
-  }
-
-  applyTheme() {
-    let localValue = this.getLocalTheme();
-    let enableDarkMode =
-      localValue === 'dark' ||
-      (localValue === null &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches);
-    document.documentElement.classList.toggle('dark', enableDarkMode);
-  }
-
-  getLocalTheme() {
-    return localStorage.getItem(this.storagePath);
   }
 
   setLocalTheme(theme) {
@@ -43,6 +39,32 @@ export default class extends Controller {
     } else {
       localStorage.setItem(this.storagePath, theme);
     }
-    this.applyTheme();
+  }
+
+  getLocalTheme() {
+    let localValue = localStorage.getItem(this.storagePath);
+    if (localValue === null) {
+      return 'auto';
+    } else if (localValue === 'dark') {
+      return 'dark';
+    } else {
+      return 'light';
+    }
+  }
+
+  applyTheme() {
+    let theme = this.getLocalTheme();
+
+    let enableDarkMode =
+      theme === 'dark' || (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches);
+    document.documentElement.classList.toggle('dark', enableDarkMode);
+
+    this.buttonTargets.forEach((target) => {
+      let active = target.getAttribute('data-' + this.identifier + '-target').includes(theme);
+      this.classActive.forEach((className) => target.classList.toggle(className, active));
+      this.classInactive.forEach((className) => target.classList.toggle(className, !active));
+      // todo : use something more robust than lastChildElement
+      target.lastElementChild.classList.toggle('hidden', !active);
+    });
   }
 }
